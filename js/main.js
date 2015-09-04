0e323afc85160ebdcae15df4cc5870ab3a10b2fc
Richard McCutchan <richard.mccutchan@deliveryhero.com>
2015-09-04 11:32:07 +0200

    added animation on icons

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//fire off animation
+var app = function () {
+//    var animation = $(this).attr('fade-out-top');
+    $('.food-icon').toggleClass('fade-out-top');
+}
+
+//sticky element
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top + (-160);
+    if (window_top > div_top) {
+        $('.overlay').css('visibility', 'hidden');
+        $('.overlay-scroll').animate({
+            "top": "0",
+            "height": 130
+        }, 360, "swing");
+        $('.overlay-scroll').show();
+        $('.overlay-scroll').css('position', 'fixed');
+    } else {
+        $('.overlay').css('visibility', 'visible');
+        $('.overlay-scroll').animate({
+            "top": "-160px",
+            "height": 130
+        }, 1, "swing");
+        $('.overlay-scroll').hide();
+        $('.overlay-scroll').css('position', 'relative');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+//slider
+$(function () {
+    $(".rslides").responsiveSlides({
+        auto: true, // Boolean: Animate automatically, true or false
+        speed: 500, // Integer: Speed of the transition, in milliseconds
+        timeout: 4000, // Integer: Time between slide transitions, in milliseconds
+        pager: false, // Boolean: Show pager, true or false
+        nav: false, // Boolean: Show navigation, true or false
+        random: true, // Boolean: Randomize the order of the slides, true or false
+        pause: false, // Boolean: Pause on hover, true or false
+        pauseControls: true, // Boolean: Pause when hovering controls, true or false
+        prevText: "Previous", // String: Text for the "previous" button
+        nextText: "Next", // String: Text for the "next" button
+        maxwidth: "", // Integer: Max-width of the slideshow, in pixels
+        navContainer: "", // Selector: Where controls should be appended to, default is after the 'ul'
+        manualControls: "", // Selector: Declare custom pager navigation
+        namespace: "rslides", // String: Change the default namespace used
+        before: function () {}, // Function: Before callback
+        after: function () {} // Function: After callback
+    });
+});
+
+$(document).ready(app);
